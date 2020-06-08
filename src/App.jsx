a7d7474c95f05c5d92583c0a3d3f2e8d1e3ce6fc
jordantom <jordan.tom88@gmail.com>
2020-06-08 11:21:14 -0700

    Add events to calculator buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ function App() {
   const [reset, setReset] = useState(true);
   const [operator, setOperator] = useState('');
 
-  const operatorChange = (op, e) => {
-    e.preventDefault();
-    setOperator(op);
-    setReset(true);
-  }
+  const operatorChange = (op) => (
+    e => {
+      e.preventDefault();
+      setOperator(op);
+      setReset(true);
+    }
+  )
 
   const clear = e => {
     e.preventDefault();
@@ -22,22 +24,26 @@ function App() {
     setOperator('');
   }
 
-  const updateValue = (num, e) => {
-    e.preventDefault();
-    if(operator === '') {
-      if(reset) {
-        setValue(num);
-        setReset(false);
+  const updateValue = (num) => (
+    e => {
+      e.preventDefault();
+      if(operator === '') {
+        if(reset) {
+          setValue(num);
+          setReset(false);
+        } else {
+          setValue(value + num)
+        }
       } else {
         if(reset) {
           setSecondVal(num);
           setReset(false);
         } else {
-          setSecondVal(setSecondVal + num);
+          setSecondVal(secondVal + num)
         }
       }
     }
-  }
+  )
 
   const result = () => {
     let result;
@@ -70,34 +76,34 @@ function App() {
         <div className='input' id='input'>{ operator === '' ? value : secondVal}</div>
         <div className='buttons'>
           <div className='operators'>
-            <div>+</div>
-            <div>-</div>
-            <div>x</div>
-            <div>/</div>
+            <div onClick={operatorChange('+')}>+</div>
+            <div onClick={operatorChange('-')}>-</div>
+            <div onClick={operatorChange('*')}>&times;</div>
+            <div onClick={operatorChange('/')}>&divide;</div>
           </div>
           <div className='leftPanel'>
             <div className='numbers'>
-              <div>7</div>
-              <div>8</div>
-              <div>9</div>
+              <div onClick={updateValue('7')}>7</div>
+              <div onClick={updateValue('8')}>8</div>
+              <div onClick={updateValue('9')}>9</div>
             </div>
             <div className='numbers'>
-              <div>4</div>
-              <div>5</div>
-              <div>6</div>
+              <div onClick={updateValue('4')}>4</div>
+              <div onClick={updateValue('5')}>5</div>
+              <div onClick={updateValue('6')}>6</div>
             </div>
             <div className='numbers'>
-              <div>1</div>
-              <div>2</div>
-              <div>3</div>
+              <div onClick={updateValue('1')}>1</div>
+              <div onClick={updateValue('2')}>2</div>
+              <div onClick={updateValue('3')}>3</div>
             </div>
             <div className='numbers'>
-              <div>0</div>
-              <div>.</div>
-              <div>C</div>
+              <div onClick={updateValue('0')}>0</div>
+              <div onClick={updateValue('.')}>.</div>
+              <div id='clear' onClick={clear}>C</div>
             </div>
           </div>
-          <div className='equal' id='result'>=</div>
+          <div className='equal' id='result' onClick={result}>=</div>
         </div>
       </div>
     </div>
